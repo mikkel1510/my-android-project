dfc07be8c3d57d8a87f2dcd392e989eda0a04482
Mikkel Nielsen <mikkel1510@live.dk>
2025-10-30 17:15:28 +0100

    List implemented with rememberSaveable

--- app/src/main/java/com/example/gimmedamoney/MainActivity.kt
+++ app/src/main/java/com/example/gimmedamoney/MainActivity.kt
@@ -1,23 +1,40 @@
 package com.example.gimmedamoney
 
-import android.R
-import android.app.Person
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.heightIn
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.ui.graphics.Color;
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
-import androidx.compose.material3.Scaffold
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateListOf
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.gimmedamoney.ui.theme.GimmeDaMoneyTheme
 
 class MainActivity : ComponentActivity() {
@@ -26,8 +43,7 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             GimmeDaMoneyTheme {
-                    PersonList()
-                    AddMember()
+                    MembersScreen()
                 }
             }
         }
@@ -38,8 +54,7 @@ data class Member(
 
 
 @Composable
-fun PersonList() {
-    val members = remember { mutableStateListOf<Member>() }
+fun PersonList(members: MutableList<Member>) {
     Column {
         for(member in members){
             Person(member.name)
@@ -49,8 +64,20 @@ fun PersonList() {
 
 
 @Composable
-fun Person(name: String, modifier: Modifier = Modifier){
-    Row(){
+fun Person(name: String){
+    Row(modifier = Modifier
+        .padding(10.dp)
+        .border(width = 2.dp, color = Color.LightGray, shape = RoundedCornerShape(8.dp))
+        .padding(10.dp)
+        .width(150.dp),
+        verticalAlignment = Alignment.CenterVertically,
+        horizontalArrangement = Arrangement.spacedBy(8.dp)
+    ){
+        Image(
+            modifier = Modifier.size(25.dp),
+            painter = painterResource(id = R.drawable.user_icon),
+            contentDescription = "User icon"
+        )
         Text(
             name
         )
@@ -58,9 +85,49 @@ fun Person(name: String, modifier: Modifier = Modifier){
 }
 
 @Composable
-fun AddMember(members: MutableList<Member>){
-    var member = Member("Bruun")
-    Button(onClick = { members.add(member) }) { }
+fun AddMemberBar(members: MutableList<Member>, modifier: Modifier = Modifier){
+    var memberName by rememberSaveable() { mutableStateOf("") }
+
+    Row (modifier
+        .fillMaxWidth()
+        .padding(12.dp)
+        .height(75.dp),
+        verticalAlignment = Alignment.CenterVertically,
+        horizontalArrangement = Arrangement.spacedBy(12.dp)
+    ){
+        OutlinedTextField(
+            value = memberName,
+            onValueChange = { memberName = it },
+            label = { Text("Name") },
+            singleLine = true,
+            modifier = Modifier
+                .weight(1f)
+                .heightIn(min = 56.dp)
+        )
+        Button(
+            onClick = { members.add(Member(memberName)); memberName = "" },
+            modifier = Modifier
+                .width(75.dp)
+        ) {
+            Text("Add")
+        }
+    }
+}
+
+
+@Composable
+fun MembersScreen(){
+    val members = rememberSaveable { mutableStateListOf<Member>() }
+    Column (
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(16.dp),
+        verticalArrangement = Arrangement.SpaceBetween,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        PersonList(members)
+        AddMemberBar(members, Modifier.fillMaxWidth())
+    }
 }
 
 
@@ -69,7 +136,6 @@ fun AddMember(members: MutableList<Member>){
 @Composable
 fun GreetingPreview() {
     GimmeDaMoneyTheme {
-        PersonList()
-        AddMember()
+        MembersScreen()
     }
 }
