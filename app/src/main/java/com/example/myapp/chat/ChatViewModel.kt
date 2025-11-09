e6ac6a253a9b8bbdced485c85810826cb4c8506d
Mikkel Nielsen <mikkel1510@live.dk>
2025-11-09 22:59:13 +0100

    Users fetched from API - Users can be added from list - Common 'User' data class

--- app/src/main/java/com/example/myapp/chat/ChatViewModel.kt
+++ app/src/main/java/com/example/myapp/chat/ChatViewModel.kt
@@ -1,4 +1,4 @@
-package com.example.myapp
+package com.example.myapp.chat
 
 import androidx.compose.runtime.mutableStateListOf
 import androidx.lifecycle.ViewModel
--- app/src/main/java/com/example/myapp/chat/GroupChat.kt
+++ app/src/main/java/com/example/myapp/chat/GroupChat.kt
@@ -1,4 +1,4 @@
-package com.example.myapp
+package com.example.myapp.chat
 
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
